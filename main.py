6763c9cd1f7ef48eb6e1653947dc1fb87d3a5182
a-rust <107306810+a-rust@users.noreply.github.com>
2026-05-20 23:54:01 -0700

    Update notebook overview

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def _(mo):
         items=[
             mo.md("# Polynomial Irreducibility via Eisenstein's Criterion"),
             mo.md(
-                r"### This is an interactive notebook that illustrates [Eisenstein's criterion](https://en.wikipedia.org/wiki/Eisenstein's_criterion)"
+                r"### This is an interactive notebook that applies [Eisenstein's criterion](https://en.wikipedia.org/wiki/Eisenstein's_criterion) to test a randomly generated polynomial $q(x)\in \mathbb{Z}[x]$ for [irreducibility](https://en.wikipedia.org/wiki/Irreducible_polynomial) over $\mathbb{Q}$"
             ),
-            mo.md("## Overview of Theorem"),
+            mo.md("# Eisenstein's Criterion Overview"),
             mo.md(
                 r"### Let $q(x) = a_0 + a_1(x) + a_2(x^2) + ... + a_n(x^n)$, $a_i \in \mathbb{Z}$"
             ),
             mo.md(
-                r"### $q(x)$ is irreducible over $\mathbb{Q}$ if there exists a prime number $p\in \mathbb{Z}$ such that all three conditions are met:"
+                r"### Eisenstein's criterion states that $q(x)$ is irreducible over $\mathbb{Q}$ if there exists a prime number $p\in \mathbb{Z}$ such that all three conditions are met:"
             ),
             mo.md(r"### $p$ divides each $a_i$ for $0 \leq i < n$"),
             mo.md(r"### $p$ does not divide $a_n$"),
